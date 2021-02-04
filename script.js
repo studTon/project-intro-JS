7ffba57b4313e54a6b0fb373c4ad477751e12f85
studTon <bra.17.ess@gmail.com>
2021-02-04 20:45:02 -0300

    Lesson one: about simple output commands I built index.html I made style.css I began script.js Added image in the article

---
+++ script.js
@@ -0,0 +1,10 @@
+/*
+    Bio details
+*/
+var nome = "studTon"
+var idade = 21
+var frase = "To code is awesome!!"
+//alert("Hello World!")
+//alert("Hello " + nome)
+console.log("Hello World!!")
+console.log(frase)
