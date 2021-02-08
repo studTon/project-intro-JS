bad92e5fd0fe05061c587b8494a5acec41f8c697
studTon <bra.17.ess@gmail.com>
2021-02-08 19:26:20 -0300

    Finished basics

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
     Strings
     Output values or strings
 */
+
+/*
 var nome = "studTon";
 var idade = 21;
 var frase = "To code is awesome!!";
@@ -10,9 +12,11 @@ var frase = "To code is awesome!!";
 //alert("Hello " + nome);
 console.log("Hello World!!");
 console.log(frase);
+*/
 /*
     basic arithmetic
  */
+/*
 idade = idade + 8;
 idade = idade - 2;
 idade = idade / 6;
@@ -21,7 +25,7 @@ console.log(idade)
 console.log(8 % 3);
 var montante = idade * 35;
 console.log(montante);
-
+*/
 /*
     lists and dictionaries
  */
@@ -33,15 +37,21 @@ fruit = {"name":"apple", "colour":"red", "weight":"180g"}
 console.log(cars[2]); //print third element
 console.log(fruit.name) //print name key
 
-cars = l
+cars.push("minivan");
+var removed = cars.pop()
+
+console.log(cars)
 
 /*
     loops and Date functionalities
  */
+/*
+var idade = 10;
 var iCount = 0;
  while(iCount < idade) 
  {
     console.log(iCount);
+    iCount++;
  }
 
 for (iCount = 5; iCount < idade - 3; iCount++) 
@@ -50,4 +60,5 @@ for (iCount = 5; iCount < idade - 3; iCount++)
 }
 
 var tempoAtual = new Date();
-console.log(tempoAtual.getHours);
+console.log(tempoAtual.getHours());
+*/
