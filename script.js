b9896e1638e39faabf19f986c753c252a857b41c
studTon <bra.17.ess@gmail.com>
2021-02-08 19:03:33 -0300

    Added more lesson notes to JS file

--- script.js
+++ script.js
@@ -1,10 +1,53 @@
 /*
-    Bio details
+    Var declaration
+    Strings
+    Output values or strings
 */
-var nome = "studTon"
-var idade = 21
-var frase = "To code is awesome!!"
-//alert("Hello World!")
-//alert("Hello " + nome)
-console.log("Hello World!!")
-console.log(frase)
+var nome = "studTon";
+var idade = 21;
+var frase = "To code is awesome!!";
+//alert("Hello World!");
+//alert("Hello " + nome);
+console.log("Hello World!!");
+console.log(frase);
+/*
+    basic arithmetic
+ */
+idade = idade + 8;
+idade = idade - 2;
+idade = idade / 6;
+idade = idade % 1;
+console.log(idade)
+console.log(8 % 3);
+var montante = idade * 35;
+console.log(montante);
+
+/*
+    lists and dictionaries
+ */
+
+cars = ['sport', 'classic', 'sedan', 'hatch']
+
+fruit = {"name":"apple", "colour":"red", "weight":"180g"}
+
+console.log(cars[2]); //print third element
+console.log(fruit.name) //print name key
+
+cars = l
+
+/*
+    loops and Date functionalities
+ */
+var iCount = 0;
+ while(iCount < idade) 
+ {
+    console.log(iCount);
+ }
+
+for (iCount = 5; iCount < idade - 3; iCount++) 
+{
+    console.log(iCount);       
+}
+
+var tempoAtual = new Date();
+console.log(tempoAtual.getHours);
